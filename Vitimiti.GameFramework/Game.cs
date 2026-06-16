977572595b40f953eb17e42cd37192f3937aadd0
Victor Matia <vmatir@gmail.com>
2026-06-16 21:31:53 +0100

    Add a system to show messageboxes during unhandled exceptions.

--- Vitimiti.GameFramework/Game.cs
+++ Vitimiti.GameFramework/Game.cs
@@ -19,13 +19,18 @@
 
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Vitimiti.GameFramework.Internals;
+using static Vitimiti.GameFramework.NativeInterop.Ffi;
 
 namespace Vitimiti.GameFramework;
 
-public class Game(ILoggerFactory? loggerFactory = null) : IDisposable
+public abstract partial class Game(ILoggerFactory? loggerFactory = null) : IDisposable
 {
     private static int _isRunning; // 0 = false, 1 = true
+
+    private readonly ILogger<Game> _logger =
+        loggerFactory?.CreateLogger<Game>() ?? NullLoggerFactory.Instance.CreateLogger<Game>();
 
     private int _ownsRunLock; // 0 = false, 1 = true
     private SdlContext? _sdlContext = new(loggerFactory);
@@ -57,6 +62,60 @@ public class Game(ILoggerFactory? loggerFactory = null) : IDisposable
         }
     }
 
+    private void SetUnhandledExceptionHandlers()
+    {
+        AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+                LogUnhandledException(_logger, ex);
+                try
+                {
+                    if (
+                        !SDL_ShowSimpleMessageBox(
+                            SDL_MESSAGEBOX_ERROR,
+                            "Unhandled Exception",
+                            ex.ToString(),
+                            SDL_Window.Null
+                        )
+                    )
+                    {
+                        LogUnableToShowMessageBox(_logger, SDL_GetError());
+                    }
+                }
+                catch (Exception ex2)
+                {
+                    LogExceptionWhenShowingMessageBox(_logger, ex2);
+                }
+            }
+            else
+            {
+                var exceptionObjectString =
+                    e.ExceptionObject?.ToString() ?? "Unknown unhandled exception object.";
+
+                LogUnknownUnhandledException(_logger, exceptionObjectString);
+                try
+                {
+                    if (
+                        !SDL_ShowSimpleMessageBox(
+                            SDL_MESSAGEBOX_ERROR,
+                            "Unknown Unhandled Exception",
+                            exceptionObjectString,
+                            SDL_Window.Null
+                        )
+                    )
+                    {
+                        LogUnableToShowMessageBox(_logger, SDL_GetError());
+                    }
+                }
+                catch (Exception ex2)
+                {
+                    LogExceptionWhenShowingMessageBox(_logger, ex2);
+                }
+            }
+        };
+    }
+
     [MemberNotNull(nameof(_sdlContext))]
     private void Initialize()
     {
@@ -68,6 +127,7 @@ public class Game(ILoggerFactory? loggerFactory = null) : IDisposable
             );
         }
 
+        SetUnhandledExceptionHandlers();
         _sdlContext.Initialize();
     }
 
@@ -105,4 +165,35 @@ public class Game(ILoggerFactory? loggerFactory = null) : IDisposable
         Dispose(disposing: true);
         GC.SuppressFinalize(this);
     }
+
+    [LoggerMessage(
+        EventId = 1000,
+        Level = LogLevel.Critical,
+        Message = "Unhandled exception occurred."
+    )]
+    private static partial void LogUnhandledException(ILogger logger, Exception exception);
+
+    [LoggerMessage(
+        EventId = 1001,
+        Level = LogLevel.Critical,
+        Message = "Unknown unhandled exception object occurred: {Obj}."
+    )]
+    private static partial void LogUnknownUnhandledException(ILogger logger, object obj);
+
+    [LoggerMessage(
+        EventId = 1002,
+        Level = LogLevel.Error,
+        Message = "Unable to show message box: {Error}"
+    )]
+    private static partial void LogUnableToShowMessageBox(ILogger logger, string error);
+
+    [LoggerMessage(
+        EventId = 1003,
+        Level = LogLevel.Error,
+        Message = "Exception when showing message box."
+    )]
+    private static partial void LogExceptionWhenShowingMessageBox(
+        ILogger logger,
+        Exception exception
+    );
 }
